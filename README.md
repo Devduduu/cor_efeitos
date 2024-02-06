88c253b698d7a287f2ae947b11970181c3390635
Devduduu <dudupy0523@gmail.com>
2024-02-05 21:33:02 -0300

    criei o html

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cor_efeitos
- 
+
+Esse simples codigo, feito com html, css e javasript. Fiz esse codigo mais pra testar uma ideia, que estava na minha mente.
---
+++ script.js
@@ -0,0 +1,20 @@
+const Yellow =  document.getElementById('btn-amarelo')
+const Vermelho = document.getElementById('btn-vermelho')
+const Black = document.getElementById('btn-preto')
+
+function TrocarYellow(){
+    document.querySelector("body").setAttribute('class', 'amarelo')
+}
+
+function TrocarRed(){
+    document.querySelector("body").setAttribute('class', 'vermelho')
+}
+
+function TrocarBlack(){
+    document.querySelector("body").setAttribute('class', 'preto')
+}
+
+
+Yellow.addEventListener('click', TrocarYellow)
+Vermelho.addEventListener('click', TrocarRed)
+Black.addEventListener('click', TrocarBlack)
